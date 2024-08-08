1fc014776b7dea02d680e4ff4eb7b216548aa5aa
Uladzislau Prusevich <92080958+UladzislauP@users.noreply.github.com>
2024-08-08 18:31:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+var map = L.map('map', {
+    center: [52.23189489585977, 21.00729567750159],
+    zoom: 17,
+    minZoom: 15,
+    maxZoom: 19,
+    maxBounds: [
+        [52.228, 21.002],
+        [52.236, 21.012]  
+    ],
+    maxBoundsViscosity: 1.0
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var latlngs = [
+    [52.23344, 21.008481],
+    [52.233558, 21.00902],
+    [52.232701, 21.009519],
+    [52.232582, 21.008988]
+];
+
+var polygon = L.polygon(latlngs, {
+    color: 'red',
+    weight: 2,
+    opacity: 1,
+    fillColor: 'red',
+    fillOpacity: 0.1 
+}).addTo(map);
+
+var centerLatLng = polygon.getBounds().getCenter();
+
+L.marker(centerLatLng).addTo(map)
+    .bindPopup('Muzeum Sztuki Nowoczesnej - nowa siedziba', { closeButton: false }) 
+    .openPopup();
+
+map.fitBounds(polygon.getBounds());
+
+L.Control.Info = L.Control.extend({
+    onAdd: function(map) {
+        var infoButton = L.DomUtil.create('div', 'leaflet-control-zoom-info');
+        infoButton.innerHTML = '<span class="material-symbols-outlined">info</span>';
+        L.DomEvent.on(infoButton, 'click', function() {
+            window.location.href = 'index_info.html';
+        });
+        return infoButton;
+    },
+    onRemove: function(map) {
+    }
+});
+
+L.control.info = function(opts) {
+    return new L.Control.Info(opts);
+}
+
+L.control.info({ position: 'topleft' }).addTo(map);
+
+function redirectToIndex(buttonType) {
+    if (buttonType === 'yes') {
+        document.querySelector('.button.yes').style.backgroundColor = 'green';
+    } else if (buttonType === 'no') {
+        document.querySelector('.button.no').style.backgroundColor = 'red';
+    }
+    setTimeout(function() {
+        window.location.href = 'index2.html';
+    }, 300);
+}
