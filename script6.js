88bdfa9026b0228260e04740156608930992569a
Uladzislau Prusevich <92080958+UladzislauP@users.noreply.github.com>
2024-08-17 22:32:23 +0200

    Update script6.js

--- script6.js
+++ script6.js
@@ -79,7 +79,7 @@ L.Control.Info = L.Control.extend({
         var infoButton = L.DomUtil.create('div', 'leaflet-control-zoom-info');
         infoButton.innerHTML = '<span class="material-symbols-outlined">info</span>';
         L.DomEvent.on(infoButton, 'click', function() {
-            window.location.href = 'index_info.html';
+            window.location.href = 'index6_info.html';
         });
         return infoButton;
     },
